d015479bc933ebbcb075e1e63116dfa7ab20bbd4
k.durga Nagaraja Reddy <kanchamdurga@gmail.com>
2022-12-31 20:32:56 +0530

    first commit

---
+++ day-1/index1.js
@@ -0,0 +1,12 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+let name="Durga"
+console.log(name)
+var fatherName="Nagaraja Reddy"
+console.log(fatherName)
+const motherName="Nirmala"
+console.log(motherName)
---
+++ day-1/index3.js
@@ -0,0 +1,29 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+// Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspirationvar 
+name="durga";
+var school="zphs belupalli";
+var grade=9;
+var section="B" ;
+var rollnumber=8;
+console.log("*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*");
+console.log("*          report card        *");
+console.log("          "+"~~~~~~~~~~~~~~~"+"    ")
+console.log("@ name: ",name+"                "+"@");
+console.log("* school: ",school+"     "+"*");
+console.log("@ grade: ",grade+"                   "+"@");
+console.log("* section: ",section+"                 "+"*");
+console.log("@ rollnumber: ",rollnumber+"              "+"@");
+
+console.log("@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@")
+console.log("*              MARKS          *");
+console.log("              "+"~~~~~~~"+"    ")
+var subject="marks"
+var maths=90
+var english=89
+var social=95
+console.log("@ subject : ",subject+"            "+"@");
+console.log("* maths: ",maths+"                  "+"*");
+console.log("@ english: ",english+"                "+"@");
+console.log("* social: ",social+"                 "+"*");
+console.log("*@*@*@*@*@*@*@*@*@*@*@*@*@*@*@*")
